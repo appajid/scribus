6db98f0b00ac3e1eb2b37cbcbb41dcf249b5ece2
Appaji Ambarisha Darbha <ambarisha@gmail.com>
2026-09-13 01:09:58 +0530

    Expand Windows project portability checks

--- ci/portable/check_windows_projects.cmake
+++ ci/portable/check_windows_projects.cmake
@@ -16,6 +16,10 @@ set(FORK_APPLICATION_FILES
 	scribus/styles/objectstyle.h
 	scribus/stylequickapplymodel.cpp
 	scribus/stylequickapplymodel.h
+	scribus/ui/smobjectstyle.cpp
+	scribus/ui/smobjectstyle.h
+	scribus/ui/smobjectstylewidget.cpp
+	scribus/ui/smobjectstylewidget.h
 	scribus/ui/dynamicvariableinsert.cpp
 	scribus/ui/dynamicvariableinsert.h
 	scribus/ui/dynamicvariablemanager.cpp
@@ -26,26 +30,39 @@ set(FORK_APPLICATION_FILES
 	scribus/ui/widgets/inspector_header.cpp
 	scribus/ui/widgets/inspector_header.h)
 
-foreach(MSVC_VERSION IN ITEMS 2019 2022 2026)
-	set(PROJECT_DIR "${SCRIBUS_ROOT}/win32/msvc${MSVC_VERSION}/scribus-main")
-	file(READ "${PROJECT_DIR}/Scribus.vcxproj" PROJECT_CONTENTS)
-	file(READ "${PROJECT_DIR}/Scribus.vcxproj.filters" FILTER_CONTENTS)
+set(FORK_SCRIPT_PLUGIN_FILES
+	scribus/plugins/scriptplugin/cmdhistory.cpp
+	scribus/plugins/scriptplugin/cmdhistory.h
+	scribus/plugins/scriptplugin/cmdobjectstyleimport.cpp
+	scribus/plugins/scriptplugin/cmdobjectstyleimport.h
+	scribus/plugins/scriptplugin/cmdobjectstylemanagement.cpp
+	scribus/plugins/scriptplugin/cmdobjectstylemanagement.h)
 
-	foreach(SOURCE_FILE IN LISTS FORK_APPLICATION_FILES)
+function(check_project_files MSVC_VERSION PROJECT_DIR_NAME PROJECT_FILE_NAME)
+	set(PROJECT_DIR "${SCRIBUS_ROOT}/win32/msvc${MSVC_VERSION}/${PROJECT_DIR_NAME}")
+	file(READ "${PROJECT_DIR}/${PROJECT_FILE_NAME}.vcxproj" PROJECT_CONTENTS)
+	file(READ "${PROJECT_DIR}/${PROJECT_FILE_NAME}.vcxproj.filters" FILTER_CONTENTS)
+
+	foreach(SOURCE_FILE IN LISTS ARGN)
 		string(REPLACE "/" "\\" WINDOWS_SOURCE_FILE "${SOURCE_FILE}")
 		set(PROJECT_REFERENCE "..\\..\\..\\${WINDOWS_SOURCE_FILE}")
 		string(FIND "${PROJECT_CONTENTS}" "${PROJECT_REFERENCE}" PROJECT_INDEX)
 		if(PROJECT_INDEX EQUAL -1)
 			message(FATAL_ERROR
-				"${SOURCE_FILE} is missing from the MSVC ${MSVC_VERSION} project")
+				"${SOURCE_FILE} is missing from the MSVC ${MSVC_VERSION} ${PROJECT_FILE_NAME} project")
 		endif()
 
 		string(FIND "${FILTER_CONTENTS}" "${PROJECT_REFERENCE}" FILTER_INDEX)
 		if(FILTER_INDEX EQUAL -1)
 			message(FATAL_ERROR
-				"${SOURCE_FILE} is missing from the MSVC ${MSVC_VERSION} filters")
+				"${SOURCE_FILE} is missing from the MSVC ${MSVC_VERSION} ${PROJECT_FILE_NAME} filters")
 		endif()
 	endforeach()
+endfunction()
+
+foreach(MSVC_VERSION IN ITEMS 2019 2022 2026)
+	check_project_files(${MSVC_VERSION} scribus-main Scribus ${FORK_APPLICATION_FILES})
+	check_project_files(${MSVC_VERSION} scriptplugin scriptplugin ${FORK_SCRIPT_PLUGIN_FILES})
 endforeach()
 
-message(STATUS "All fork application files are registered in every Windows project")
+message(STATUS "All fork application and Scripter files are registered in every Windows project")
